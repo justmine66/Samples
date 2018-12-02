dfea3d88d98579936bbbc54ce9e27451c4948058
justmine <justmine@DESKTOP-UILU6SC>
2018-12-02 07:27:23 -0800

    添加SpanStructTypeSample

---
+++ BenchmarkTest/SpanTest/SpanStructTypeSample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SpanTest
+{
+    class SpanStructTypeSample
+    {
+        public static void Test()
+        {
+            var value = new StructType<int>();
+            Parse(value);
+        }
+
+        static void Parse(IEnumerable<int> collection) { }
+    }
+
+    struct StructType<T> : IEnumerable<T>
+    {
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
