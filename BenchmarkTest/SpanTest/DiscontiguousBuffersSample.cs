8d28abb490d4cba778c8f5b0ce95794017bcfefa
justmine <justmine@DESKTOP-UILU6SC>
2018-12-08 06:05:46 -0800

    添加不连续缓冲区例子

---
+++ BenchmarkTest/SpanTest/DiscontiguousBuffersSample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Buffers;
+
+namespace SpanTest
+{
+    public class DiscontiguousBuffersSample
+    {
+        public void SingleSegmentSequences()
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            var sequence = new ReadOnlySequence<int>(array, 1, 3);// sequence = { 2, 3, 4 }
+
+            ReadOnlyMemory<int> memory = array;
+            sequence = new ReadOnlySequence<int>(memory);
+            sequence = sequence.Slice(1, 3);// sequence = { 2, 3, 4 }
+        }
+
+        public void LikeSingleSegmentSequences()
+        {
+            int[] array1 = { 1, 2, 3 };
+            int[] array2 = { 4, 5, 6 };
+
+            var startSegment = new Segment<int>(array1);
+            var endSegment = startSegment.Add(array2);
+
+            var sequence = new ReadOnlySequence<int>(startSegment, 1, endSegment, 3);
+        }
+    }
+}
---
+++ BenchmarkTest/SpanTest/MemorySample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Buffers;
+
+namespace SpanTest
+{
+    public class MemorySample
+    {
+        private static MemoryPool<byte> _pool =
+        MemoryPool<byte>.Shared;
+
+        public void Worker(Memory<byte> buffer)
+        {
+
+        }
+
+        public void UserCode()
+        {
+            using (IMemoryOwner<byte> buffer = _pool.Rent(1024))
+            {
+                Worker(buffer.Memory);
+            }
+        }
+    }
+}
