172ca30a19fae25b3de86fe257ff508925c9ab3a
据说甜蜜呢 <lijun@geekbuy.com.cn>
2018-12-14 16:12:43 +0800

    添加文件

---
+++ BenchmarkTest/SpanTest/MemorySample.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpanTest
+{
+    public class MemorySample
+    {
+        private static MemoryPool<byte> _pool = MemoryPool<byte>.Shared;
+
+        public void Worker(Memory<byte> buffer)
+        {
+            var str = new Memory<char>();
+        }
+
+        static async Task<uint> ChecksumReadAsync(Memory<byte> buffer, Stream stream)
+        {
+            var bytesRead = await stream.ReadAsync(buffer);
+            return SafeSum(buffer.Span.Slice(0, bytesRead));
+        }
+
+        static uint SafeSum(Span<byte> buffer)
+        {
+            uint sum = 0;
+            foreach (var t in buffer)
+            {
+                sum += t;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ BenchmarkTest/SpanTest/SpanParsingSample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpanTest
+{
+    public class SpanParsingSample
+    {
+        public void Old()
+        {
+            var json = new byte[100];
+            var text = Encoding.UTF8.GetString(json);//非常昂贵：复制，转码，对象分配。
+            int.TryParse(text, out var value);
+        }
+    }
+}
