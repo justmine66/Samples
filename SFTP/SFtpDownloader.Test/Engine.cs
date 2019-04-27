8834f79288235560a6372dca41b7e8de54b71965
justmine <justmine@DESKTOP-UILU6SC>
2019-04-27 22:22:54 +0800

    添加nuget配置

--- SFTP/SFtpDownloader.Test/Engine.cs
+++ SFTP/SFtpDownloader.Test/Engine.cs
@@ -8,20 +8,20 @@ namespace SFtpDownloader.Test
 {
     public class Engine : IHostedService
     {
-private readonly ILogger _logger;
-private readonly IFilesDownloader _downloader;
+        private readonly ILogger _logger;
+        private readonly IFilesDownloader _downloader;
 
-public Engine(ILogger<Engine> logger, IFilesDownloader downloader)
-{
-    _logger = logger;
-    _downloader = downloader;
-}
+        public Engine(ILogger<Engine> logger, IFilesDownloader downloader)
+        {
+            _logger = logger;
+            _downloader = downloader;
+        }
 
-public async Task StartAsync(CancellationToken cancellationToken)
-{
-    var files = await _downloader.DownloadAsync(1);
-    _logger.LogInformation($"The files downloaded: {files.Aggregate((x, y) => $"{x},{y}")}.");
-}
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            var files = await _downloader.DownloadAsync(1);
+            _logger.LogInformation($"The files downloaded: {files.Aggregate((x, y) => $"{x},{y}")}.");
+        }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
