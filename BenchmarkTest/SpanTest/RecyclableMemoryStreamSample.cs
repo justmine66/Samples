8d3a91257b57c121464a6787a1a09e9d31d9d32e
justmine <lijun@geekbuy.com.cn>
2019-04-28 09:10:27 +0800

    添加RecyclableMemoryStreamSample

---
+++ BenchmarkTest/SpanTest/RecyclableMemoryStreamSample.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.IO;
+
+namespace SpanTest
+{
+    public class RecyclableMemoryStreamSample
+    {
+        private static readonly RecyclableMemoryStreamManager Pool = new RecyclableMemoryStreamManager();
+
+        public static void Test()
+        {
+            var sourceBuffer = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 };
+            using (var stream = Pool.GetStream(nameof(RecyclableMemoryStreamSample.Test)))
+            {
+                stream.Write(sourceBuffer, 0, sourceBuffer.Length);
+            }
+        }
+    }
+}
