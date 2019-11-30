5796113537955fce1a7a798620bb3e0767c44e98
justmine <lijun@geekbuy.com.cn>
2019-11-30 09:45:10 +0800

    1

--- Aop/Aop.demo.AspnetCore/Controllers/HomeController.cs
+++ Aop/Aop.demo.AspnetCore/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
 using Aop.demo.AspnetCore.Attritbutes;
 using Aop.demo.AspnetCore.Domain;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DiagnosticAdapter;
 
 namespace Aop.demo.AspnetCore.Controllers
 {
@@ -15,11 +18,37 @@ namespace Aop.demo.AspnetCore.Controllers
 
         public IActionResult Index()
         {
+            DiagnosticListener.AllListeners.Subscribe(listener =>
+            {
+                Console.WriteLine(listener.Name);
+                if (listener.Name == "HttpHandlerDiagnosticListener")
+                {
+                    listener.SubscribeWithAdapter(new HttpClientListener());
+                }
+            });
+
             Console.WriteLine(Description);
 
             Car.Fire();
 
             return View();
         }
+
+        private sealed class HttpClientListener
+        {
+            [DiagnosticName("System.Net.Http.HttpRequestOut.Start")]
+            public void OnSend(HttpRequestMessage request) => AddDefaultHeaders(request);
+
+            [DiagnosticName("System.Net.Http.HttpRequestOut")]
+            public void OnSend() { }
+        }
+
+        private static void AddDefaultHeaders(HttpRequestMessage request)
+        {
+            request.Headers.Add("x-www-foo", "123");
+            request.Headers.Add("x-www-bar", "456");
+            request.Headers.Add("x-www-baz", "789");
+        }
+
     }
 }
